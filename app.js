d1de3d8f3e669324f585f97cc05a6b8516a990d6
VishnuKaku <kakuvishnu959@gmail.com>
2025-03-04 17:06:39 +0530

    Security

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const swaggerDoc = require('./swagger.json');
 const routes = require('./routes');
 const cartRoutes = require('./routes/cart');
 const { sequelize } = require('./models/index');
-
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
